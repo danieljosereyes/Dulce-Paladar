c37435448dba118e092d8a53836c1826e19ac697
Daniel Reyes <danieljosereyes0000@gmail.com>
2022-06-22 19:50:02 -0400

    javascript con comentario

--- js/main.js
+++ js/main.js
@@ -1,83 +1,116 @@
-//let usuario = prompt(`ingrese usuario/admin`)
+class producto {
+    constructor(id, nombre, precio, cantidad){
+        this.id = id
+        this.nombre = nombre
+        this.precio = precio
+        this.cantidad = cantidad
+}}
 
-//while (usuario !== `admin`) {
-//	alert(`usuario incorrecto!`)
-//	usuario = prompt(`ingrese el usuario/admin`)
-//}
+const productos = [
+    new producto(1, "croassan", 1, 10),
+    new producto(2, "flan", 0.5, 7),
+    new producto(3, "tarta de crema y frutas", 2, 2),
+    new producto(4, "pastel de naranja", 2, 3),
+]
 
-const saludar = (nombre) => {
-	let frase = `hola ${nombre}! Como estas?`
-	console.log(frase)
+const contenedorProductos = document.getElementById('contenedor--Productos')
+contenedorProductos.className = "blue"
+
+function mostrarProductos() {
+    productos.forEach((item) => {
+        const div = document.createElement('div')
+
+        div.innerHTML = `<h3 class="blue">${item.nombre}</h3>
+                         <p class="green">Precio: ${item.precio}</p>
+                         <p class="yellowgreen">Cantidad: ${item.cantidad}</p>    
+                        `
+        contenedorProductos.appendChild(div)
+    })
 }
 
-const validarCliente = (edad)=>{
-	edad = prompt(`cual es tu edad?`)
-	if (edad >= 18) {
-		alert ("Puedes pasar.")
-	} else {
-		alert("no puedes comprar sin un representante.")
-	}
-}
-//saludar(prompt(`cual es tu nombre`))
-//validarCliente()
+const textElement = document.getElementById("bienvenido")
+const usuario = prompt(`usuario`)
+textElement.innerHTML = `Bienvenido ${usuario}`
 
+mostrarProductos()
 
-//lista de supermercado
-//let lista = prompt(`cuantos productos vas a comprar?`)
-//let listaSupermercado = []
+const comentario = [];
 
-//for (i = 0; i < lista; i++){
-//	listaSupermercado[i] = [prompt(`Cuales son los productos a comprar?`)]
-//}
+const inputContenedor = document.createElement("div");
+const input = document.createElement("input");
 
+input.classList.add("input");
+const contenedorComentario = document.querySelector("#contenedor--comentarios");
 
-//carro de compra
-const datos = {
-    metodos: {
-        encontrar: (torta) => {
-            return datos.items.encontrar(item => item.torta = torta);
-        },
-        remover: (items) => {
-            items.forEach(item => {
-                const product = datos.metodos.find(item.torta)
-                product.item - item.item;
-            });
-            console.log(datos)
-        },
-    },
-    items: [
-        {
-            torta: 0,
-            titulo: "tarta de crema y frutas",
-            precio: 7,
-            item: 3,
-        },
-        {
-            torta: 1,
-            titulo: "flan vegano",
-            precio: 7,
-            item: 2,
-            },  
-        {
-            torta: 2,
-            titulo: "pastel de crema",
-            precio: 7,
-            item: 5,
-        },
-    ]
+contenedorComentario.appendChild(inputContenedor);
+inputContenedor.appendChild(input);
+
+input.addEventListener("keydown", (e) => {
+  teclaEnter(e, null);
+});
+
+function teclaEnter(e, enter) {
+  if (e.key === "Enter" && e.target.value != "") {
+    const nuevoComentario = {
+      texto: e.target.value,
+      meGusta: 0,
+      respuesta: [],
+    };
+    if (enter === null) {
+      comentario.unshift(nuevoComentario);
+    } else {
+      enter.respuesta.unshift(nuevoComentario);
+    }
+    e.target.value = "";
+    contenedorComentario.innerHTML = "";
+    contenedorComentario.appendChild(inputContenedor);
+    hacercomentario(comentario, contenedorComentario);
+  }
 }
 
+function hacercomentario(arreglo, bloque) {
+  arreglo.forEach((elementos) => {
+    const comentarioContenido = document.createElement("div");
+    comentarioContenido.classList.add("comment-container");
 
-const usuario = {
-    nombre: "daniel",
-    ci: 24145860,
-    correo: "danieljosereyes0000@gmail.com",
-    edad: 26,
+    const hacerComentario = document.createElement("div");
+    hacerComentario.classList.add("responses-container");
+    
+    const botonResponder = document.createElement("button");
+    botonResponder.textContent = "Responder";
+
+    const botonMeGusta = document.createElement("button");
+    botonMeGusta.textContent = "Me Gusta";
+/*     botonResponder.addEventListener("click", (e) => {
+      const nuevoInput = inputContenedor.cloneNode(true);
+      nuevoInput.value = "";
+      nuevoInput.focus();
+      nuevoInput.addEventListener("keydown", (e) => {
+        teclaEnter(e, elementos);
+      });
+      comentarioContenido.insertBefore(nuevoInput, hacerComentario);
+    });
+    botonMeGusta.addEventListener("click", (e) => {
+      elementos.meGusta++;
+      botonMeGusta.textContent = `${
+        elementos.meGusta > 0 ? elementos.meGusta : ""
+      } Likes`;
+    }); */
+    const divContent = document.createElement("div");
+    divContent.textContent = elementos.texto;
+
+    const divActions = document.createElement("div");
+    //commentContainer.appendChild(document.createTextNode(element.text));
+    comentarioContenido.appendChild(divContent);
+    comentarioContenido.appendChild(divActions);
+
+    divActions.appendChild(botonResponder);
+    divActions.appendChild(botonMeGusta);
+
+    comentarioContenido.appendChild(hacerComentario);
+    if (elementos.respuesta.length > 0) {
+      hacercomentario(elementos.respuesta, hacerComentario);
+    }
+    bloque.appendChild(comentarioContenido);
+  });
 }
-
-usuario.edad += 1
-
-let raza = "piel"
-usuario[raza] = "ninguna"
-
-console.log(usuario)
